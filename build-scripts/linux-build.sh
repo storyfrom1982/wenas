3f4b548a0207daef0f9504e9d4721a05025df175
kangliyong <storyfrom1982@gmail.com>
2022-11-05 19:05:31 +0800

    clear

--- build-scripts/linux-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-PREFIX=/tmp/$(uname -i)
-
-CPPFLAGS="-D__SR_LOG_DEBUG__ -D___SR_MALLOC___ -D___SR_MALLOC_DEBUG___" \
-CFLAGS="-I$PREFIX/include -L$PREFIX/lib" \
-./configure --prefix=$PREFIX \
-	--disable-shared
