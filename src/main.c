cdf89a15fd3a208718bb0cb189e75f0102bff30e
storyfrom1982 <storyfrom1982@gmail.com>
2022-11-08 16:14:06 +0800

    number 64bit to block

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
 	fprintf(stdout, "hello world\n");
 
+#ifdef __LITTLE_ENDIAN__
+	fprintf(stdout, "__LITTLE_ENDIAN__\n");
+#endif
+
 
 	float fa = 1234.5678;
 	float fb;
@@ -51,13 +55,39 @@ int main(int argc, char *argv[])
 	// 数字的低位在内存的低位，小映射到小为小端。
 	//
 
-	Lineardb block;
-	int32_t n, i = 0x1a1b1c1d;
-	
-	block = __int32_to_block(i);
-	n = __block_to_int32(&block);
-	
-	fprintf(stdout, "%x %x %x %x %x %x\n", i, n, block.byte[1], block.byte[2], block.byte[3], block.byte[4]);
+	Lineardb block, *b = &block;
+
+	int16_t n16, i16 = 0x1a1b;
+	block = __number16_to_block(i16);
+	n16 = __block_to_number16(b).i;
+
+	uint32_t size =	__block_size(b);
+
+	fprintf(stdout, "size ===== %u\n", size);
+	fprintf(stdout, "%x %x %x %x %x %x\n", i16, n16, b->byte[1], b->byte[2], b->byte[3], b->byte[4]);
+
+
+	int32_t n32, i32 = 0x1a1b1c1d;
+	block = __number32_to_block(i32);
+	n32 = __block_to_number32(b).i;
+
+	size =	__block_size(b);
+
+	fprintf(stdout, "size ===== %u\n", size);
+	fprintf(stdout, "%x %x %x %x %x %x\n", i32, n32, b->byte[1], b->byte[2], b->byte[3], b->byte[4]);
+
+	int64_t n64, n = 0x1a1b1c1d1a1b1c1d;
+	block = __number64_to_block(n);
+	n64 = __block_to_number64(b).i;
+	// n64 = ((Number64)(((int64_t)(b)->byte[8]) | ((int64_t)(b)->byte[7] << 8) | ((int64_t)(b)->byte[6] << 16) | ((int64_t)(b)->byte[5] << 24) 
+	// 	| ((int64_t)(b)->byte[4] << 32) | ((int64_t)(b)->byte[3] << 40) | ((int64_t)(b)->byte[2] << 48) | ((int64_t)(b)->byte[1] << 56))).i;
+	// n64 = (((b)->byte[0] & 0x80) ? (Number64)((b)->byte[8] | (b)->byte[7] << 8 | (b)->byte[6] << 16 | (b)->byte[5] << 24 | (b)->byte[4] << 32 | (b)->byte[3] << 40 | (b)->byte[2] << 48 | (b)->byte[1] << 56)
+    //         : (Number64)((b)->byte[1] | (b)->byte[2] << 8 | (b)->byte[3] << 16 | (b)->byte[4] << 24 | (b)->byte[5] << 32 | (b)->byte[6] << 40 | (b)->byte[7] << 48 | (b)->byte[8] << 56));
+
+	size =	__block_size(b);
+
+	fprintf(stdout, "size ===== %u\n", size);
+	fprintf(stdout, "%llx %llx %x %x %x %x %x %x %x %x\n", n, n64, b->byte[1], b->byte[2], b->byte[3], b->byte[4], b->byte[5], b->byte[6], b->byte[7], b->byte[8]);
 
 	return 0;
 }
--- src/sys/struct/linear_data_block.h
+++ src/sys/struct/linear_data_block.h
@@ -1,27 +1,135 @@
 #ifndef __LINEAR_DATA_BLOCK_H__
 #define __LINEAR_DATA_BLOCK_H__
 
+
 #include <stdint.h>
 
 enum {
-    BLOCK_TYPE_NUMBER_8BIT = 1,
-    BLOCK_TYPE_NUMBER_16BIT = 2,
-    BLOCK_TYPE_NUMBER_32BIT = 4,
-    BLOCK_TYPE_NUMBER_64BIT = 8,
-    BLOCK_TYPE_BLOCK_SIZE_8BIT,
-    BLOCK_TYPE_BLOCK_SIZE_16BIT,
-    BLOCK_TYPE_BLOCK_SIZE_32BIT,
-    BLOCK_TYPE_BLOCK_SIZE_64BIT,
-    BLOCK_TYPE_OBJECT_SIZE_64BIT,
-    BLOCK_TYPE_BLOCK_ARRAY_SIZE_64BIT
+    BLOCK_TYPE_NUMBER_8BIT = 0x01,
+    BLOCK_TYPE_NUMBER_16BIT = 0x02,
+    BLOCK_TYPE_NUMBER_32BIT = 0x04,
+    BLOCK_TYPE_NUMBER_64BIT = 0x08,
+    BLOCK_TYPE_STRING_8BIT = 0x10,
+    BLOCK_TYPE_STRING_16BIT = 0x20,
+    BLOCK_TYPE_STRING_32BIT = 0x40,
+    BLOCK_BYTES_ORDER_BIG_ENDIAN = 0x80
 };
 
 typedef struct linear_data_block {
     char byte[16];
 }Lineardb;
 
+typedef union number_16bit{
+    int16_t i;
+    uint16_t u;
+}Number16;
 
-#define __block_to_int32(b)   (int32_t)((b)->byte[1] | (b)->byte[2] << 8 | (b)->byte[3] << 16 | (b)->byte[4] << 24)
-#define __int32_to_block(i)   (Lineardb){BLOCK_TYPE_NUMBER_32BIT, (char)((i) & 0xff), (char)((i) >> 8 & 0xff), (char)(((i) >> 16) & 0xff), (char)(((i) >> 24) & 0xff)}
+typedef union number_32bit{
+    float f;
+    int32_t i;
+    uint32_t u;
+}Number32;
+
+typedef union number_64bit{
+    double f;
+    int64_t i;
+    uint64_t u;
+}Number64;
+
+
+
+#ifdef __LITTLE_ENDIAN__
+
+#   define __number16_to_block(n)  (Lineardb){BLOCK_TYPE_NUMBER_16BIT, (char)((n) & 0xff), (char)((n) >> 8 & 0xff)}
+#   define __block_to_number16(b) \
+            (((b)->byte[0] & 0x80) ? (Number32)((b)->byte[2] | (b)->byte[1] << 8) : (Number32)((b)->byte[1] | (b)->byte[2] << 8))
+
+#   define __number32_to_block(n)  (Lineardb){BLOCK_TYPE_NUMBER_32BIT, (char)((n) & 0xff), (char)((n) >> 8 & 0xff), (char)(((n) >> 16) & 0xff), (char)(((n) >> 24) & 0xff)}
+#   define __block_to_number32(b) \
+            (((b)->byte[0] & 0x80) ? (Number32)((b)->byte[4] | (b)->byte[3] << 8 | (b)->byte[2] << 16 | (b)->byte[1] << 24) : (Number32)((b)->byte[1] | (b)->byte[2] << 8 | (b)->byte[3] << 16 | (b)->byte[4] << 24))
+
+#   define __number64_to_block(n) \
+            (Lineardb){ \
+				BLOCK_TYPE_NUMBER_64BIT, \
+				(((char*)&(n))[0]), (((char*)&(n))[1]), (((char*)&(n))[2]), (((char*)&(n))[3]), \
+				(((char*)&(n))[4]), (((char*)&(n))[5]), (((char*)&(n))[6]), (((char*)&(n))[7]) \
+			}
+
+#   define __block_to_number64(b) \
+            (((((char*)(b))[0]) & 0x80) \
+                ? (Number64)( \
+                    ((int64_t)((char*)(b))[8]) \
+                    | ((int64_t)((char*)(b))[7] << 8) \
+                    | ((int64_t)((char*)(b))[6] << 16) \
+                    | ((int64_t)((char*)(b))[5] << 24) \
+                    | ((int64_t)((char*)(b))[4] << 32) \
+                    | ((int64_t)((char*)(b))[3] << 40) \
+                    | ((int64_t)((char*)(b))[2] << 48) \
+                    | ((int64_t)((char*)(b))[1] << 56) \
+                ) \
+                : (Number64)( \
+                    ((int64_t)((char*)(b))[1]) \
+                    | ((int64_t)((char*)(b))[2] << 8) \
+                    | ((int64_t)((char*)(b))[3] << 16) \
+                    | ((int64_t)((char*)(b))[4] << 24) \
+                    | ((int64_t)((char*)(b))[5] << 32) \
+                    | ((int64_t)((char*)(b))[6] << 40) \
+                    | ((int64_t)((char*)(b))[7] << 48) \
+                    | ((int64_t)((char*)(b))[8] << 56) \
+                ) \
+			)
+
+#else //__BIG_ENDIAN__
+
+#   define __number16_to_block(n)  (Lineardb){BLOCK_TYPE_NUMBER_16BIT | BLOCK_BYTES_ORDER_BIG_ENDIAN, (char)((n) & 0xff), (char)((n) >> 8 & 0xff)}
+#   define __block_to_number16(b) \
+            (((b)->byte[0] & 0x80) ? (Number32)((b)->byte[1] | (b)->byte[2] << 8) : (Number32)((b)->byte[2] | (b)->byte[1] << 8))
+
+#   define __number32_to_block(n)  (Lineardb){BLOCK_TYPE_NUMBER_32BIT | BLOCK_BYTES_ORDER_BIG_ENDIAN, \
+            (char)((n) & 0xff), (char)((n) >> 8 & 0xff), (char)(((n) >> 16) & 0xff), (char)(((n) >> 24) & 0xff)}
+#   define __block_to_number32(b) \
+            (((b)->byte[0] & 0x80) ? (Number64)((b)->byte[1] | (b)->byte[2] << 8 | (b)->byte[3] << 16 | (b)->byte[4] << 24) : (Number64)((b)->byte[4] | (b)->byte[3] << 8 | (b)->byte[2] << 16 | (b)->byte[1] << 24))
+
+#   define __number64_to_block(n) \
+            (Lineardb){ \
+				BLOCK_TYPE_NUMBER_64BIT | BLOCK_BYTES_ORDER_BIG_ENDIAN, \
+				(((char*)&(n))[0]), (((char*)&(n))[1]), (((char*)&(n))[2]), (((char*)&(n))[3]), \
+				(((char*)&(n))[4]), (((char*)&(n))[5]), (((char*)&(n))[6]), (((char*)&(n))[7]) \
+			}
+
+#   define __block_to_number64(b) \
+            (((((char*)(b))[0]) & 0x80) \
+                ? (Number64)( \
+                    ((int64_t)((char*)(b))[1]) \
+                    | ((int64_t)((char*)(b))[2] << 8) \
+                    | ((int64_t)((char*)(b))[3] << 16) \
+                    | ((int64_t)((char*)(b))[4] << 24) \
+                    | ((int64_t)((char*)(b))[5] << 32) \
+                    | ((int64_t)((char*)(b))[6] << 40) \
+                    | ((int64_t)((char*)(b))[7] << 48) \
+                    | ((int64_t)((char*)(b))[8] << 56) \
+                ) \
+                : (Number64)( \
+                    ((int64_t)((char*)(b))[8]) \
+                    | ((int64_t)((char*)(b))[7] << 8) \
+                    | ((int64_t)((char*)(b))[6] << 16) \
+                    | ((int64_t)((char*)(b))[5] << 24) \
+                    | ((int64_t)((char*)(b))[4] << 32) \
+                    | ((int64_t)((char*)(b))[3] << 40) \
+                    | ((int64_t)((char*)(b))[2] << 48) \
+                    | ((int64_t)((char*)(b))[1] << 56) \
+                ) \
+			)
+
+#endif
+
+
+#define __block_size(b)     \
+            ((b)->byte[0] & (~0x80)) > 8 ? (((b)->byte[0] & (~0x80)) == BLOCK_TYPE_STRING_8BIT) \
+			: (((b)->byte[0] & (~0x80)) == BLOCK_TYPE_STRING_8BIT) ? ((b)->byte[1] + 1) \
+            : (((b)->byte[0] & (~0x80)) == BLOCK_TYPE_STRING_16BIT) ? ((__block_to_number16(b)).u + 1) \
+            : (((b)->byte[0] & (~0x80)) == BLOCK_TYPE_STRING_32BIT) ? ((__block_to_number32(b)).u + 1) \
+            : ((b)->byte[0] + 1)
+
 
 #endif
