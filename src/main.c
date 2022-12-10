5a92321900ec102cc681a5c8616ccf2e86f9ddbe
kangliyong <storyfrom1982@gmail.com>
2022-12-10 10:11:39 +0800

    BuBuWeiYing

--- src/main.c
+++ src/main.c
@@ -1,17 +1,17 @@
 #include "env/env.h"
 
 
-extern void linearkv_test();
-extern void lineardb_test();
-extern void lineardb_pipe_test();
-extern void thread_test();
-extern void task_queue_test();
-extern void heap_test();
-extern void malloc_test();
-extern void crash_backtrace_test();
-extern void file_system_test();
-extern void logger_test();
-extern int test_error(const char *test);
+// extern void linearkv_test();
+// extern void lineardb_test();
+// extern void lineardb_pipe_test();
+// extern void thread_test();
+// extern void task_queue_test();
+// extern void heap_test();
+// extern void malloc_test();
+// extern void crash_backtrace_test();
+// extern void file_system_test();
+// extern void logger_test();
+// extern int test_error(const char *test);
 
 static void log_print(int level, const char *tag, const char *debug, const char *log)
 {
@@ -20,34 +20,38 @@ static void log_print(int level, const char *tag, const char *debug, const char 
 
 int main(int argc, char *argv[])
 {
-    env_backtrace_setup();
-	env_logger_start("/tmp/log", log_print);
+    // env_backtrace_setup();
+	// env_logger_start("/tmp/log", log_print);
 
-	LOGD("TEST", "hello world\n");
+	// LOGD("TEST", "hello world\n");
+    fprintf(stdout, "hello world\n");
 
 #ifdef __PL64__
-    LOGD("TEST", "__PL64__\n");
+    // LOGD("TEST", "__PL64__\n");
+    fprintf(stdout, "__PL64__\n");
 #endif
 
 #ifdef __LITTLE_ENDIAN__
-	LOGD("TEST", "__LITTLE_ENDIAN__\n");
+	// LOGD("TEST", "__LITTLE_ENDIAN__\n");
+    fprintf(stdout, "__LITTLE_ENDIAN__\n");
 #else
-	LOGD("TEST", "__BIG_ENDIAN__\n");
+	// LOGD("TEST", "__BIG_ENDIAN__\n");
+    fprintf(stdout, "__BIG_ENDIAN__\n");
 #endif
 
 
 
-    lineardb_test();
-    linearkv_test();
-    lineardb_pipe_test();
-    thread_test();
-    task_queue_test();
-    heap_test();
-    file_system_test();
+    // lineardb_test();
+    // linearkv_test();
+    // lineardb_pipe_test();
+    // thread_test();
+    // task_queue_test();
+    // heap_test();
+    // file_system_test();
 
 
-    logger_test();
-    malloc_test();
+    // logger_test();
+    // malloc_test();
 
 
 	return 0;
