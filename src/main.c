90164263559745ce16d9f4223db933605017e566
storyfrom1982 <storyfrom1982@gmail.com>
2023-02-27 17:49:18 +0800

    add c mutex

--- src/main.c
+++ src/main.c
@@ -1,45 +1,117 @@
 #include "env/env.h"
+#include "env/mutex.h"
 
-extern void test();
+
+struct targs {
+    ___mutex_ptr mtx;
+    ___atom_bool *testTrue;
+};
+
+
+static void* sthread(void *p)
+{
+    struct targs *targ = (struct targs *)p;
+    __logi("lock %u", *targ->testTrue);
+
+    while (1)
+    {
+        bool ret = ___atom_try_lock(targ->testTrue);
+        // __logi("__atom_try_lock %u", *targ->testTrue);
+        if (ret){
+            __logi("__atom_try_lock %u", *targ->testTrue);
+            break;
+        }
+    }
+    __logi("lock %u", *targ->testTrue);
+
+    __logi("thread enter");
+    ___lock lk = ___mutex_lock(targ->mtx);
+    __logi("sleep_until");
+    
+    ___mutex_timer(targ->mtx, lk, 1000000000);
+
+    ___mutex_notify(targ->mtx);
+    __logi("notify");
+    ___mutex_wait(targ->mtx, lk);
+    ___mutex_unlock(targ->mtx, lk);
+    __logi("exit");
+    return NULL;
+}
 
 int main(int argc, char *argv[])
 {
     env_backtrace_setup();
     env_logger_start("./tmp/log", NULL);
 	
-    __logi("hello world\n");
-    __logi("time %llu clock %llu\n", env_time(), env_clock());
-    char buf[1024] = {0};
-    uint64_t n = env_strftime(buf, 1024, env_time() / NANO_SECONDS);
-    buf[n] = '\0';
-    __logi("time %s\n", buf);
+    char text[1024] = {0};
 
-    n = env_strftime(buf, 1024, env_clock() / NANO_SECONDS);
-    buf[n] = '\0';
-    __logi("clock %s\n", buf);
+    uint64_t millisecond = env_time();
+    ___sys_strftime(text, 1024, millisecond / NANO_SECONDS);
+    __logi("c time %s", text);
 
-#ifdef __PL64__
-    // LOGD("TEST", "__PL64__\n");
-    __logi("__PL64__\n");
-#endif
+    millisecond = env_clock();
+    ___sys_strftime(text, 1024, millisecond / NANO_SECONDS);
+    __logi("c clock %s", text);
 
-#ifdef __LITTLE_ENDIAN__
-	// LOGD("TEST", "__LITTLE_ENDIAN__\n");
-    __logi("__LITTLE_ENDIAN__\n");
-#else
-	// LOGD("TEST", "__BIG_ENDIAN__\n");
-    __logi("__BIG_ENDIAN__\n");
-#endif
+    ___atom_size size = 10;
+    ___atom_sub(&size, 5);
+    __logi("___atom_sub %lu", size);
 
-#ifdef ENV_HAVA_BACKTRACE
-    __logi("ENV_HAVA_BACKTRACE\n");
-#endif
+    ___atom_add(&size, 15);
+    __logi("___atom_add %lu", size);
 
-    __logi("__uint64 size: %d\n", sizeof(uint64_t));
-    __logi("int32_t size: %d\n", sizeof(int32_t));
-    __logi("bool size: %d\n", sizeof(bool));
+    ___atom_bool testTrue = false;
 
-    // test();
+    if (___set_true(&testTrue)){
+        __logi("set false to true");
+    }
+
+    if (___is_true(&testTrue)){
+        __logi("is true");
+    }
+
+    if (___set_false(&testTrue)){
+        __logi("set true to false");
+    }
+
+    if (___is_false(&testTrue)){
+        __logi("is false");
+    }
+
+    ___atom_lock(&testTrue);
+    __logi("is lock");
+
+    ___mutex_ptr mtx = ___mutex_create();
+
+    ___lock lk = ___mutex_lock(mtx);
+
+    // ___atom_bool *tt = &testTrue;
+    // std::cout << "testTrue: " << *tt << std::endl;
+
+    struct targs targ;
+    targ.mtx = mtx;
+    targ.testTrue = &testTrue;
+
+    __thread_ptr tid = ___thread_create(sthread, &targ);
+
+    ___mutex_timer(mtx, lk, 1000000000);
+    ___atom_unlock(&testTrue);
+
+    // std::this_thread::sleep_until(std::chrono::steady_clock::now() + 1000ms);
+    
+    __logi("waitting");
+    ___mutex_wait(mtx, lk);
+    __logi("wake");
+    
+    ___mutex_broadcast(mtx);
+    ___mutex_unlock(mtx, lk);
+
+    __logi("join thread %lu", ___thread_id());
+    ___thread_join(tid);
+
+    ___mutex_release(mtx);
+
+    __logi("exit");
 
 	return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,39 @@
-#include "env/env.h"
+#include "env/mutex.h"
 
-#include <thread>
-#include <atomic>
-#include <mutex>
-#include <chrono>
-#include <condition_variable>
-
-#include <iostream>
+extern "C" {
+    #include "env/env.h"
+}
 
 
-
-struct CxxMutex {
-    
-public:
-    using CxxLock = std::unique_lock<std::mutex>;
-
-    CxxLock lock(){
-        CxxLock _lock(_mutex);
-        return _lock;
-    }
-
-    void unlock(CxxLock &_lock){
-        _lock.unlock();
-    }
-
-    void wait(CxxLock &_lock){
-        _cond.wait(_lock);
-    }
-
-    void timer(CxxLock &_lock, uint64_t nanos){
-        _cond.wait_until(_lock, std::chrono::system_clock::now() + std::chrono::nanoseconds(nanos));
-    }
-
-    void notify(){
-        _cond.notify_one();
-    }
-
-    void broadcast(){
-        _cond.notify_all();
-    }
-
-private:
-    std::mutex _mutex;
-    std::condition_variable _cond;
+struct targs {
+    ___mutex_ptr mtx;
+    ___atom_bool *testTrue;
 };
 
 
-typedef struct CxxMutex*   ___mutex_ptr;
-
-#define ___mutex_create()                  new CxxMutex()
-#define ___mutex_release(mtx)              delete (mtx)
-#define ___mutex_lock(mtx)                 (mtx)->lock()
-#define ___mutex_notify(mtx)               (mtx)->notify()
-#define ___mutex_broadcast(mtx)            (mtx)->broadcast()
-#define ___mutex_wait(mtx, lock)           (mtx)->wait((lock))
-#define ___mutex_timer(mtx, lock, delay)   (mtx)->timer((lock), (delay))
-#define ___mutex_unlock(mtx, lock)         (mtx)->unlock((lock))
-
-
-
-typedef std::atomic<size_t>     ___atom_bool;
-typedef std::atomic<size_t>     ___atom_size;
-
-
-#define ___atom_sub(x, y)       (x)->fetch_sub((y))
-#define ___atom_add(x, y)       (x)->fetch_add((y))
-
-static inline bool ___set_true(___atom_bool *obj)
-{
-    size_t ___atom_false = false;
-    return obj->compare_exchange_strong(___atom_false, true);
-}
-
-static inline bool ___set_false(___atom_bool *obj)
-{
-    size_t ___atom_true = true;
-    return obj->compare_exchange_strong(___atom_true, false);
-}
-
-#define	___is_true(x)           (x)->load()
-#define	___is_false(x)          (x)->load()
-
-#define ___atom_lock(x) \
-    while(!___set_true(x)) \
-        std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::nanoseconds(100))
-#define ___atom_try_lock(x)     ___set_true(x)
-#define ___atom_unlock(x)       ___set_false(x)
-
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    char text[1024] = {0};
+
+    uint64_t millisecond = env_time();
+    ___sys_strftime(text, 1024, millisecond / NANO_SECONDS);
+    std::cout << "c time: " << text << std::endl;
+
+    millisecond = ___sys_time();
+    ___sys_strftime(text, 1024, millisecond / NANO_SECONDS);
+    std::cout << "cxx time: " << text << std::endl;
+
+    millisecond = env_clock();
+    ___sys_strftime(text, 1024, millisecond / NANO_SECONDS);
+
+    std::cout << "c clock: " << text << std::endl;
+
+    millisecond = ___sys_clock();
+    ___sys_strftime(text, 1024, millisecond / NANO_SECONDS);
+    std::cout << "cxx clock: " << text << std::endl;
+
     ___atom_size size = 10;
     ___atom_sub(&size, 5);
     std::cout << "___atom_sub: " << size << std::endl;
@@ -125,30 +69,37 @@ int main(int argc, char *argv[])
     // ___atom_bool *tt = &testTrue;
     // std::cout << "testTrue: " << *tt << std::endl;
 
-    std::thread s_th([mtx, &testTrue](){
-        std::cout << "tt: " << testTrue << std::endl;
-        while (1)
-        {
-            bool ret = ___atom_try_lock(&testTrue);
-            // std::cout << "__atom_try_lock: " << ret << std::endl;
-            if (ret){
-                // std::cout << "__atom_try_lock: " << ret << std::endl;
-                break;
-            }
-        }
-        std::cout << "tt: " << testTrue << std::endl;
+    struct targs targ;
+    targ.mtx = mtx;
+    targ.testTrue = &testTrue;
 
-        std::cout << "thread enter\n";
-        auto lk = ___mutex_lock(mtx);
-        std::cout << "sleep_until\n";
-        ___mutex_timer(mtx, *(CxxMutex::CxxLock*)(&lk), 1000000000);
-        // std::this_thread::sleep_until(std::chrono::steady_clock::now() + 1000ms);
-        ___mutex_notify(mtx);
-        std::cout << "notify\n";
-        ___mutex_wait(mtx, lk);
-        ___mutex_unlock(mtx, lk);
-        std::cout << "exit\n";
-    });
+    __thread_ptr tid = ___thread_create([](void *p) -> void*
+        {
+            struct targs *targ = (struct targs *)p;
+            std::cout << "tt: " << *targ->testTrue << std::endl;
+            while (1)
+            {
+                bool ret = ___atom_try_lock(targ->testTrue);
+                std::cout << "__atom_try_lock: " << *targ->testTrue << std::endl;
+                if (ret){
+                    std::cout << "__atom_try_lock: " << *targ->testTrue << std::endl;
+                    break;
+                }
+            }
+            std::cout << "tt: " << *targ->testTrue << std::endl;
+
+            std::cout << "thread enter\n";
+            auto lk = ___mutex_lock(targ->mtx);
+            std::cout << "sleep_until\n";
+            ___mutex_timer(targ->mtx, *(CxxMutex::CxxLock*)(&lk), 1000000000);
+            // std::this_thread::sleep_until(std::chrono::steady_clock::now() + 1000ms);
+            ___mutex_notify(targ->mtx);
+            std::cout << "notify\n";
+            ___mutex_wait(targ->mtx, lk);
+            ___mutex_unlock(targ->mtx, lk);
+            std::cout << "exit\n";
+            return nullptr;
+    }, &targ);
 
     ___mutex_timer(mtx, *(CxxMutex::CxxLock*)(&lk), 1000000000);
     ___atom_unlock(&testTrue);
@@ -160,7 +111,8 @@ int main(int argc, char *argv[])
     ___mutex_broadcast(mtx);
     ___mutex_unlock(mtx, lk);
 
-    s_th.join();
+    std::cout << "join thread " << ___thread_id() << std::endl;
+    ___thread_join(tid);
 
     ___mutex_release(mtx);
 
