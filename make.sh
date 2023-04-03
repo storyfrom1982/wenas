b5a592d52c63e0573e89b1caca9c47320f7bb91e
storyfrom1982 <storyfrom1982@gmail.com>
2023-04-03 10:40:51 +0800

    add mtpd.service

--- make.sh
+++ make.sh
@@ -5,3 +5,10 @@ cmake -B build -D CMAKE_BUILD_TYPE=$1 && cmake --build build
 # ./build/kang
 ./build/server
 # ./build/client
+
+
+
+# sudo systemctl daemon-reload
+# sudo systemctl enable mtpd
+# sudo systemctl start mtpd
+# sudo systemctl status mtpd
