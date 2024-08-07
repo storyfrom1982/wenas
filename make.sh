6c9e5b3748fd368a858901dc54e2176853e17e0f
kangliyong <storyfrom1982@gmail.com>
2024-08-07 15:52:30 +0800

    修复 on connect 时覆盖msg导致的崩溃 4

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 clear
 rm -rf build
-cmake -B build -D CMAKE_BUILD_TYPE=$3 && cmake --build build
+cmake -B build -D CMAKE_BUILD_TYPE=$5 && cmake --build build
 
-./build/$1 $2
+./build/$1 $2 $3 $4
 # ./build/xserver 47.99.146.226 9256
 
 # sudo systemctl daemon-reload
