1931cc464f41bd48617f7a2701c802549bbba7e8
storyfrom1982 <storyfrom1982@gmail.com>
2025-01-31 11:03:47 +0800

    缓冲的包不能小于32

--- src/xnet/xmsger.c
+++ src/xnet/xmsger.c
@@ -608,10 +608,10 @@ static inline void xchannel_recv_ack(xchannel_ptr channel, xpack_ptr rpack)
 
                 if (channel->threshold == 0){
                     __xlogd("back delay = %lu buf readable = %u\n", channel->back_delay, __serialbuf_readable(channel->sendbuf));
-                    xchannel_send_pack(channel);
-                    if (__serialbuf_readable(channel->sendbuf) == 0){
-                        channel->threshold = channel->serial_range;
+                    if (__serialbuf_readable(channel->sendbuf) < 32){
+                        channel->threshold = 32;
                     }
+                    xchannel_send_pack(channel);
                 }
 
                 // __ring_list_take_out(&channel->flushlist, pack);
