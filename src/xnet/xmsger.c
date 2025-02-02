e6f68d89f0601915070ee7fe8012c5b47c32c669
storyfrom1982 <storyfrom1982@gmail.com>
2025-02-02 17:49:02 +0800

    添加调试日志

--- src/xnet/xmsger.c
+++ src/xnet/xmsger.c
@@ -667,8 +667,9 @@ static inline void xchannel_recv_ack(xchannel_ptr channel, xpack_ptr rpack)
 
     }else {
 
-        // __xlogd("xchannel_recv_ack >>>>-----------> (%u) OUT OF RANGE: %u\n", channel->peer_cid, rpack->head.sn);
-        channel->resend_factor *= 1.1f;
+        
+        channel->resend_factor *= 1.15;
+        __xlogd("xchannel_recv_ack >>>>-----------> OUT OF RANGE: %u RESEND FACTOR: %f\n", rpack->head.sn, channel->resend_factor);
 
     }
 }
